2cabe8dba7cbbb3137d21201a7934b6394b39a7f
Eugene Istratov <warexify@outlook.com>
2018-05-07 18:15:59 +0300

    Add ngrok and shellcheck

---
+++ manual/goodbyedpi/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿if (Get-OSArchitectureWidth -eq 64) {
+$subfolder = '_64'
+}
+Install-ChocolateyZipPackage "$env:chocolateyPackageName" 'https://github.com/ValdikSS/GoodbyeDPI/releases/download/0.1.5rc1/goodbyedpi-0.1.5rc1.zip' "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" -Checksum '6d93cb20be60143581c4a35de962a674f804e8ee4dfa4ebf079971317b399525' -ChecksumType 'sha256' -SpecificFolder "goodbyedpi-0.1.5rc1/x86$subfolder"
---
+++ manual/shellcheck/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$original = Join-Path $content 'shellcheck-latest.exe'
+$target = Join-Path $content 'shellcheck.exe'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'shellcheck' `
+    -Url 'https://shellcheck.storage.googleapis.com/shellcheck-latest.zip' `
+    -Checksum '49c4ca6bba48185f53ddcc334967e7292f20a9b20d1cbe31c71bee71cfdd4e770be89f027d4044dda4159323ddfd3055bfb1e50e4c81d78375da9894e577f41f' `
+    -ChecksumType 'SHA512' `
+    -UnzipLocation $content
+
+Rename-Item -Path $original -NewName $target -Force | Out-Null
